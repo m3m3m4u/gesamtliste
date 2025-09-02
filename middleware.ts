efcbc88b81cda95746ccc08f6fed742adf6257eb
Matthias Gmeiner <m3m3m4u@gmail.com>
2025-09-02 21:20:03 +0200

    Restrict: Root '/' jetzt auch durch Middleware erfasst

--- middleware.ts
+++ middleware.ts
@@ -88,5 +88,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)'],
+  matcher: ['/', '/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)'],
 };
