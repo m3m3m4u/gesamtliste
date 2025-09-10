346e5b86ac2bb6b21cb9ab2118f3d77b47596668
Matthias Gmeiner <m3m3m4u@gmail.com>
2025-09-10 09:38:22 +0200

    chore: remove quiz gate and any password checks

--- src/app/optionen/layout.tsx
+++ src/app/optionen/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import QuizGate from '../QuizGate';
 
 export default function OptionenLayout({ children }: { children: React.ReactNode }) {
-  return <QuizGate>{children}</QuizGate>;
+  return <>{children}</>;
 }
